a5848afe13f3b0b10e77b96c870a8d7aef3ed599
RoyDaddy <tirran@naver.com>
2022-07-07 11:30:45 +0900

    c1/p1-detail 첨부파일 다운로드 추가

--- src/main/java/com/softj/itple/controller/ComController.java
+++ src/main/java/com/softj/itple/controller/ComController.java
@@ -1,6 +1,10 @@
 package com.softj.itple.controller;
 
 import com.softj.itple.domain.Response;
+import com.softj.itple.entity.BoardFile;
+import com.softj.itple.exception.ApiException;
+import com.softj.itple.exception.ErrorCode;
+import com.softj.itple.repo.BoardFileRepo;
 import com.softj.itple.service.CommonService;
 import com.softj.itple.util.CommonUtil;
 import lombok.RequiredArgsConstructor;
@@ -19,12 +23,28 @@ import java.util.HashMap;
 @RequiredArgsConstructor
 public class ComController {
     final private CommonService commonService;
+    final private BoardFileRepo boardFileRepo;
+
     @GetMapping("/comFileDownload/{path}")
     public void fileDownload(@PathVariable("path") String path,
                              HttpServletRequest request,
                              HttpServletResponse response, @RequestParam HashMap<String,String> search) throws Exception {
         path = path.replace("_","/");
         CommonUtil.setDisposition(path.substring(path.lastIndexOf("/") + 1), request, response);
+        byte[] data = null;
+
+        data = FileUtils.readFileToByteArray(new File(path));
+
+        IOUtils.write(data, response.getOutputStream());
+    }
+
+    @GetMapping("/boardFileDownload/{path}")
+    public void boardFileDownload(@PathVariable("path") String path,
+                             HttpServletRequest request,
+                             HttpServletResponse response, @RequestParam HashMap<String,String> search) throws Exception {
+        BoardFile file = boardFileRepo.findByUploadFileName(path).orElseThrow(() -> new ApiException(ErrorCode.DATA_NOT_FOUND));
+        path = path.replace("_","/");
+        CommonUtil.setDisposition(file.getOrgFileName(), request, response);
         byte[] data = null;
 
         data = FileUtils.readFileToByteArray(new File(path));
--- src/main/java/com/softj/itple/repo/BoardFileRepo.java
+++ src/main/java/com/softj/itple/repo/BoardFileRepo.java
@@ -1,13 +1,14 @@
 package com.softj.itple.repo;
 
-import com.softj.itple.entity.Board;
-import com.softj.itple.entity.BoardComment;
 import com.softj.itple.entity.BoardFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BoardFileRepo extends JpaRepository<BoardFile, Long>, QuerydslPredicateExecutor<BoardFile> {
+    Optional<BoardFile> findByUploadFileName(@Param("uploadFileName")String uploadFileName);
 }
