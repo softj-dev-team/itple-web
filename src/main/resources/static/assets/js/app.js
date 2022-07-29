db0f7be406ef7e6522a35da1c4678bbf559bfb72
jinsy <ilaabbcclisy@gmail.com>
2022-07-29 10:33:39 +0900

    QR촬영 후면카메라 변경, 메뉴 URL 수정

--- src/main/resources/static/assets/js/app.js
+++ src/main/resources/static/assets/js/app.js
@@ -15,8 +15,17 @@ var app = new Vue({
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
       if (cameras.length > 0) {
-        self.activeCameraId = cameras[0].id;
-        self.scanner.start(cameras[0]);
+        var selectedCam = cameras[0];
+        $.each(cameras, (i, c) => {
+          if (c.name.indexOf('back') > -1) {
+            selectedCam = c;
+            return false;
+          }
+        });
+        self.activeCameraId = selectedCam.id;
+        self.scanner.start(selectedCam);
+        //self.activeCameraId = cameras[0].id;
+        //self.scanner.start(cameras[0]);
       } else {
         console.error('No cameras found.');
       }
