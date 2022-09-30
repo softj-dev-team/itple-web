a17726fe7b6b8ddb7bbbe693d67ce938ce5ae531
jinsy <ilaabbcclisy@gmail.com>
2022-09-30 22:38:20 +0900

    포트폴리오

--- src/main/java/com/softj/itple/repo/PortfolioFileRepo.java
+++ src/main/java/com/softj/itple/repo/PortfolioFileRepo.java
@@ -1,5 +1,6 @@
 package com.softj.itple.repo;
 
+import com.softj.itple.entity.Portfolio;
 import com.softj.itple.entity.PortfolioFile;
 import com.softj.itple.entity.User;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -8,9 +9,12 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PortfolioFileRepo extends JpaRepository<PortfolioFile, Long>, QuerydslPredicateExecutor<PortfolioFile> {
+
+    List<PortfolioFile> findByPortfolio(Portfolio portfolio);
     Optional<PortfolioFile> findByUploadFileName(@Param("uploadFileName")String uploadFileName);
 }
