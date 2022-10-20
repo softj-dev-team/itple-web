38baffcabbee46f255e819c82d7c96e51d6a9bba
jinsy <ilaabbcclisy@gmail.com>
2022-10-20 17:57:33 +0900

    수업연계_첨부파일수정,과제독후감제출시상세페이지추가

--- src/main/java/com/softj/itple/controller/C2Controller.java
+++ src/main/java/com/softj/itple/controller/C2Controller.java
@@ -50,13 +50,18 @@ public class C2Controller {
         model.addAttribute("params",params);
         return "c2/c2p1-detail";
     }
-//
-//    //작성
-//    @GetMapping("/p1-write/{id}")
-//    public String p1write(@PathVariable long id, ModelMap model, SearchVO params){
-//        params.setId(id);
-//        model.addAttribute("el",c2Service.getBoard(params));
-//        model.addAttribute("params",params);
-//        return "c2/c2p1-write";
-//    }
+
+    //작성
+    @GetMapping("/p1-write/{id}")
+    public String p1write(@PathVariable long id, ModelMap model, SearchVO params, @PageableDefault(sort = "id" , direction = Sort.Direction.ASC) Pageable pageable){
+        if(Objects.isNull(params.getTaskType())) {
+            params.setTaskType(Types.TaskType.TASK);
+        }
+        params.setId(id);
+        StudentTask el = c2Service.getStudentTask(params);
+        model.addAttribute("el", el);
+        model.addAttribute("notSubmitList", c2Service.getNotSubmitList(params));
+        model.addAttribute("params",params);
+        return "c2/c2p1-write";
+    }
 }
--- src/main/java/com/softj/itple/controller/ComController.java
+++ src/main/java/com/softj/itple/controller/ComController.java
@@ -5,11 +5,13 @@ import com.softj.itple.domain.SearchVO;
 import com.softj.itple.entity.BoardFile;
 import com.softj.itple.entity.PortfolioFile;
 import com.softj.itple.entity.StudentTaskFile;
+import com.softj.itple.entity.TaskFile;
 import com.softj.itple.exception.ApiException;
 import com.softj.itple.exception.ErrorCode;
 import com.softj.itple.repo.BoardFileRepo;
 import com.softj.itple.repo.PortfolioFileRepo;
 import com.softj.itple.repo.StudentTaskFileRepo;
+import com.softj.itple.repo.TaskFileRepo;
 import com.softj.itple.service.CommonService;
 import com.softj.itple.util.CommonUtil;
 import lombok.RequiredArgsConstructor;
@@ -29,6 +31,7 @@ public class ComController {
     final private CommonService commonService;
     final private BoardFileRepo boardFileRepo;
     final private StudentTaskFileRepo studentTaskFileRepo;
+    final private TaskFileRepo taskFileRepo;
 
     final private PortfolioFileRepo portfolioFileRepo;
 
@@ -47,8 +50,8 @@ public class ComController {
 
     @GetMapping("/portfolioFileDownload/{path}")
     public void portfolioFileDownload(@PathVariable("path") String path,
-                                  HttpServletRequest request,
-                                  HttpServletResponse response, @RequestParam HashMap<String,String> search) throws Exception {
+                                      HttpServletRequest request,
+                                      HttpServletResponse response, @RequestParam HashMap<String,String> search) throws Exception {
         PortfolioFile file = portfolioFileRepo.findByUploadFileName(path).orElseThrow(() -> new ApiException(ErrorCode.DATA_NOT_FOUND));
         path = path.replace("_","/");
         CommonUtil.setDisposition(file.getOrgFileName(), request, response);
@@ -61,8 +64,8 @@ public class ComController {
 
     @GetMapping("/boardFileDownload/{path}")
     public void boardFileDownload(@PathVariable("path") String path,
-                             HttpServletRequest request,
-                             HttpServletResponse response, @RequestParam HashMap<String,String> search) throws Exception {
+                                  HttpServletRequest request,
+                                  HttpServletResponse response, @RequestParam HashMap<String,String> search) throws Exception {
         BoardFile file = boardFileRepo.findByUploadFileName(path).orElseThrow(() -> new ApiException(ErrorCode.DATA_NOT_FOUND));
         path = path.replace("_","/");
         CommonUtil.setDisposition(file.getOrgFileName(), request, response);
@@ -75,9 +78,23 @@ public class ComController {
 
     @GetMapping("/studentTaskFileDownload/{path}")
     public void studentTaskFileDownload(@PathVariable("path") String path,
-                             HttpServletRequest request,
-                             HttpServletResponse response, @RequestParam HashMap<String,String> search) throws Exception {
+                                        HttpServletRequest request,
+                                        HttpServletResponse response, @RequestParam HashMap<String,String> search) throws Exception {
         StudentTaskFile file = studentTaskFileRepo.findByUploadFileName(path).orElseThrow(() -> new ApiException(ErrorCode.DATA_NOT_FOUND));
+        path = path.replace("_","/");
+        CommonUtil.setDisposition(file.getOrgFileName(), request, response);
+        byte[] data = null;
+
+        data = FileUtils.readFileToByteArray(new File(path));
+
+        IOUtils.write(data, response.getOutputStream());
+    }
+
+    @GetMapping("/taskFileDownload/{path}")
+    public void taskFileDownload(@PathVariable("path") String path,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response, @RequestParam HashMap<String,String> search) throws Exception {
+        TaskFile file = taskFileRepo.findByUploadFileName(path).orElseThrow(() -> new ApiException(ErrorCode.DATA_NOT_FOUND));
         path = path.replace("_","/");
         CommonUtil.setDisposition(file.getOrgFileName(), request, response);
         byte[] data = null;
