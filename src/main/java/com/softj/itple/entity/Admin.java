44377d525b97665dfcc01fdb2c068124bee8e199
jinsy <ilaabbcclisy@gmail.com>
2022-10-26 17:29:41 +0900

    출결관리_휴/퇴원생반검색삭제,SMS관리_작업중

--- src/main/java/com/softj/itple/entity/Admin.java
+++ src/main/java/com/softj/itple/entity/Admin.java
@@ -33,9 +33,11 @@ public class Admin extends Auditing{
     private boolean menu7;
     private boolean menu8;
 
+    private boolean menu9;
+
 
     @Builder
-    public Admin(long id, LocalDateTime createdAt, LocalDateTime updatedAt, boolean isDeleted, String createdId, String updatedId, User user, boolean menu1, boolean menu2, boolean menu3, boolean menu4, boolean menu5, boolean menu6, boolean menu7, boolean menu8) {
+    public Admin(long id, LocalDateTime createdAt, LocalDateTime updatedAt, boolean isDeleted, String createdId, String updatedId, User user, boolean menu1, boolean menu2, boolean menu3, boolean menu4, boolean menu5, boolean menu6, boolean menu7, boolean menu8, boolean menu9) {
         super(id, createdAt, updatedAt, isDeleted, createdId, updatedId);
         this.user = user;
         this.menu1 = menu1;
@@ -46,6 +48,7 @@ public class Admin extends Auditing{
         this.menu6 = menu6;
         this.menu7 = menu7;
         this.menu8 = menu8;
+        this.menu9 = menu9;
     }
 
 }
